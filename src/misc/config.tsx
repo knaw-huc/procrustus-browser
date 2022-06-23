39a1fc3a9a6a14e6e5dc55b4d5b5f6fc3290ad05
kerim1 <kerim1@users.noreply.github.com>
2022-06-23 10:35:27 +0200

    Revert config

--- src/misc/config.tsx
+++ src/misc/config.tsx
@@ -1,5 +1,14 @@
-const serviceServer = '$REACT_APP_SERVICE_SERVER';
-const home = '$REACT_APP_HOME';
+// Development
+const serviceServer = "http://localhost:5000/";
+const home = "http://localhost:3000";
+
+// Production
+// const serviceServer = "https://gaservice.sd.di.huc.knaw.nl/";
+// const home = "https://ga.sd.di.huc.knaw.nl/";
+
+// Environment variables
+// const serviceServer = '$REACT_APP_SERVICE_SERVER';
+// const home = '$REACT_APP_HOME';
 
 export const getServiceServer = () => getVar(serviceServer) as string;
 export const getHome = () => getVar(home) as string;
